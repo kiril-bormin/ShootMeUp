22ac5289d92e67d4eb02fdd9a19b4426d319fb09
Kiril Bormin <borminnkiril@gmail.com>
2025-10-23 17:46:24 +0200

    chore(JDT) : Mise à jour du journal du travail

--- ShootMeUp/Drones/View/Obstacle.cs
+++ ShootMeUp/Drones/View/Obstacle.cs
@@ -9,7 +9,7 @@ namespace ShootMeUp
         public void Render(BufferedGraphics drawingSpace)
         {
             drawingSpace.Graphics.DrawImage(Resources.tour, X, Y, WIDTH, HEIGHT);//48, 61 valeurs par défaut, multiplié par 1,3 pour avoir un bon taille
-            drawingSpace.Graphics.DrawString(Name + " HP: " + _hp, TextHelpers.drawFont, TextHelpers.writingBrush, X - 20, Y - 25);
+            drawingSpace.Graphics.DrawString(Name + " HP: " + _hp, TextHelpers.drawFont, TextHelpers.writingBrush, X - 40 , Y - 25);
         }
     }
 }
